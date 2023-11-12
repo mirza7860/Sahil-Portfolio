6e47fedfdeb82955821f5cca83eb81efe0b682a5
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2023-11-12 08:28:14 +0530

    experience updated

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 const Experience = () => {
   return (
-    <Container>
+    <Container className="#experience">
       <Wrapper>
         <Title>Experience</Title>
         <Desc>
