a3fb08ae6a5574098e96c9e8dade464eec081dd1
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2023-11-12 08:23:10 +0530

    update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -25,7 +25,7 @@ const Navbar = () => {
           <NavLink href="#about">About</NavLink>
           <NavLink href='#skills'>Skills</NavLink>
           <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Experience</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
